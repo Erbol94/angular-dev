b6888c2380eda9b0b6c778a53411d7b286917aac
medersnv <msairakunov@gmail.com>
2024-01-17 13:29:00 +0600

    welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'smart-customs';
+  title = 'welcome to smart-customs';
 }
